34e886d7abf3b347a85799a201dbd5237385cf90
SHINSUNGJIN <pxp13716@gmail.com>
2022-06-14 10:31:50 +0900

    .

--- chap02_jsx/src/App.js
+++ chap02_jsx/src/App.js
@@ -1,4 +1,4 @@
-
+import FuncComp from './components/A01FuncComp'
 /*
 const elem = <h3>Hello World 01</h3>
 function App () {
@@ -21,7 +21,7 @@ const elem = <div>
 function App () {
   return elem;
 }
-*/
+
 
 function App () {
   return <div>
@@ -32,14 +32,60 @@ function App () {
 </div>;
 }
 
-/*
-function App() {
+function App () {
   return (
-    <div className="App">
-      <h1>Chap02 JSX</h1>
-
+    <div>
+      <h3>Hello World 05</h3>
+      <div>
+        This is App Component
+      </div>  
     </div>
   );
 }
 */
+
+function MakeDOM() {
+  return (
+    <div>
+      <h5>MakeDOM</h5>
+      <div>This is Function Component</div>
+    </div>
+  )
+}
+
+
+function App() {
+
+  // 값이 변경되도 화면 갱신이 안되는 일반 변수.
+  const name = '놀부';
+  const age = 30;
+
+  const onAdd = (x, y) => `${x} + ${y} = ${x + y}`;
+
+  return (
+    // jsx에서 요소의 속성은 JavaScript의 속성명을 따른다.
+    <div className="card-body">
+      <h1>Chap02 JSX</h1>
+
+      {/* 바인딩 표현법 - 보간법
+        JSX X가 XML이다. 따라서 XML 문법을 따른다. 종료 태그가 반드시 있어야 한다.
+      */}
+      Name: {name}<br />
+      Age: {age}<br />
+      onAdd: {onAdd(10, 20)}<br />
+      <br />
+
+      MakeDOM: <br />
+      {MakeDOM()}<br />
+      {/* 태그로 사용하기 위해서는 함수의 첫글자가 대문자로 지정해야 한다. */}
+      <MakeDOM></MakeDOM>
+      <br />
+      <br />
+      <FuncComp></FuncComp><br />
+      <br />
+      <img src="./images/tree.jpg" alt="나무 이미지" width="200"/>
+    </div>
+  );
+}
+
 export default App;
---
+++ chap02_jsx/src/components/A01FuncComp.js
@@ -0,0 +1,15 @@
+function A01FuncComp() {
+  const address = 'Seoul';
+
+  return (
+    <div>
+      <h5>A01 Function Component</h5>
+
+      <div>
+        This is Function Component<br />
+        Address: {address}
+      </div>
+    </div>
+  )
+}
+export default A01FuncComp;
