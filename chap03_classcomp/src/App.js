deb6e2894e099bbf24b21609ddc31e78a6fcb4d3
SHINSUNGJIN <pxp13716@gmail.com>
2022-06-14 11:21:53 +0900

    .

---
+++ chap03_classcomp/src/App.js
@@ -0,0 +1,13 @@
+import A01ClassProps from './components/A01ClassProps'
+
+function App() {
+  return (
+    <div className="card-body">
+      <h1>Chap03 Class Component</h1>
+
+      <A01ClassProps>This is App Content</A01ClassProps>
+    </div>
+  );
+}
+
+export default App;
---
+++ chap03_classcomp/src/components/A01ClassProps.js
@@ -0,0 +1,14 @@
+// A01ClassProps.js
+import React, { Component } from 'react'
+
+export class A01ClassProps extends Component {
+  render() {
+    return (
+      <div>
+        <h3>A01ClassProps</h3>
+      </div>
+    )
+  }
+}
+
+export default A01ClassProps
