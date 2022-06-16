200c0c4c668f4882fa8512699b2cca3c15f0eed5
SHINSUNGJIN <pxp13716@gmail.com>
2022-06-16 15:25:43 +0900

    .

--- chap08_axios/src/components/A01Axios.js
+++ chap08_axios/src/components/A01Axios.js
@@ -7,13 +7,72 @@ function A01Axios() {
     const baseURL = 'http://localhost:8080/contacts/';
     const [data, setData] = useState();
     
-    const getContactList = (no, size) => {
+    const getContactList = (no = 1, size = 10) => {
         axios.get(baseURL, {params: {pageno: no, pagesize: size}})
         .then(resp => {
             console.log(resp);
             setData(resp.data);     // 실질적인 값은 data 속성에 있음
         })
-        .catch(error => console.error(error))
+        .catch(error => setData(error))
+    }
+    const getContactListAsync = async (no = 1, size = 10) => {
+        try {
+            const resp = await axios.get(baseURL, {params: {pageno: no, pagesize: size}});
+            setData(resp.data);
+        } catch (error) {
+            setData(error);
+        }
+    }
+    const getContact = (no) => {
+        /*
+        axios.get(baseURL + no)
+        .then(resp => {
+            setData(resp.data);     // 실질적인 값은 data 속성에 있음
+        })
+        .catch(error => setData(error))
+        */
+       axios({
+        method: 'get',
+        url: baseURL + no,
+        data: ''
+       })
+       .then(resp => {
+            setData(resp.data);     // 실질적인 값은 data 속성에 있음
+        })
+        .catch(error => setData(error))
+    }
+    const addContact = () => {
+        const data = {
+            "name":"강감찬",
+            "tel":"010-2222-3339",
+            "address":"서울시"
+        }
+
+        axios.post(baseURL, data)
+        .then(resp => {
+            setData(resp.data);     // 실질적인 값은 data 속성에 있음
+        })
+        .catch(error => setData(error))
+    }
+    const updateContact = (no) => {
+        const data = {
+            "name":"이순신",
+            "tel":"010-1111-3339",
+            "address":"서울시"
+        }
+
+        axios.put(baseURL + no, data)
+        .then(resp => {
+            setData(resp.data);     // 실질적인 값은 data 속성에 있음
+        })
+        .catch(error => setData(error))
+    }
+    const deleteContact = (no) => {
+        axios.delete(baseURL + no)
+        .then(resp => {
+            setData(resp.data);     // 실질적인 값은 data 속성에 있음
+        })
+        .catch(error => setData(error))
     }
 
     useEffect(() => {
@@ -23,12 +82,12 @@ function A01Axios() {
     return (
         <div>
             <h3>A01 Axios GET</h3>
-            <button>DATA LIST</button>
-            <button>DATA LIST ASYNC</button>
-            <button>GET</button>
-            <button>ADD</button>
-            <button>UPDATE</button>
-            <button>DELETE</button><br />
+            <button onClick={() => getContactList(1, 10)}>DATA LIST</button>
+            <button onClick={() => getContactListAsync(2, 10)}>DATA LIST ASYNC</button>
+            <button onClick={() => getContact(99)}>GET</button>
+            <button onClick={addContact}>ADD</button>
+            <button onClick={() => updateContact(1655360450926)}>UPDATE</button>
+            <button onClick={() => deleteContact(1655360450926)}>DELETE</button><br />
             <br />
             
             <textarea cols="100" rows="15" defaultValue={JSON.stringify(data, '', 4)}></textarea>
