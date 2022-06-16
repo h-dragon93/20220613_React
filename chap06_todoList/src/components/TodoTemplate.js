e8e1bc6080c462c7b089bf1d711017a05186bec4
SHINSUNGJIN <pxp13716@gmail.com>
2022-06-16 10:59:33 +0900

    .

--- chap06_todoList/src/components/TodoTemplate.js
+++ chap06_todoList/src/components/TodoTemplate.js
@@ -4,7 +4,7 @@ import TodoList from './TodoList'
 
 const makeTodo = () => {
     const todos = [];
-    for(let i = 1; i <= 5; i++) {
+    for(let i = 1; i <= 5000; i++) {
         todos.push( {id: i, text: `${i}번째 할 일`, done: false} );
     }
     return todos;
